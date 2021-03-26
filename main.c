1084044e4fde7ee1509c66b0a2c5fbb319546f9c
Ian Beauregard <ian.beauregard@gmail.com>
2021-03-26 10:07:13 -0400

    Handle program arguments in main

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+int main(int argc, char* argv[])
+{
+    if (argc != 2) {
+        dprintf(STDERR_FILENO, "%s\n", "my_mouse takes exactly one argument (path to maze file)");
+        return EXIT_FAILURE;
+    }
+    printf("%s\n", argv[1]);
+    return EXIT_SUCCESS;
 }
